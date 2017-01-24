853ee274d2f9bf92ae36a2342d4c86bcaac73d64
Luka Bulatovic <luka.bulatovic.w@gmail.com>
2017-01-24 15:40:44 +0100

    Init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,31 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+var obj = new ObjectID();
+console.log(obj);
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if(err){
+    return console.log("Unable to connect to mongo db server");
+  }
+  console.log("Connected to mongo db server");
+  db.collection('Users').find({name: 'Luka'}).toArray().then((docs) => {
+    console.log('Users');
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, (err) => {
+    console.log("Unable to fetch users", err);
+  });
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('58863a8607273709e4b97c76')
+  // }).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log("Unable to fetch todos", err);
+  // });
+  // db.collection('Todos').find().count().then((count) => {
+  //   console.log(`Todos: count: ${count}`);
+  // }, (err) => {
+  //   console.log("Unable to fetch todos", err);
+  // });
+  // db.close();
+});
